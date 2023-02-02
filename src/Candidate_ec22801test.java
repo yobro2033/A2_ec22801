435c6f282fe6dde7ce9d3795bbe7eed162f3e69b
yobro2033 <mlhmytran@gmail.com>
2023-02-02 03:08:06 +0000

    updates

--- src/Candidate_ec22801test.java
+++ src/Candidate_ec22801test.java
@@ -1,5 +1,4 @@
 import java.util.Random;
-import java.util.Scanner;
 
 public class Candidate_ec22801test {
     interface Person {
@@ -88,7 +87,6 @@ public class Candidate_ec22801test {
         }
 
     }
-
     class Candidate_ac1235 extends Candidate {
 
         public String getName() {
@@ -157,7 +155,6 @@ public class Candidate_ec22801test {
         }
 
     }
-
     class Candidate_ac1236 extends Candidate {
 
         public String getName() {
@@ -247,30 +244,7 @@ public class Candidate_ec22801test {
         }
     }
 
-    public String validateInput(String input) {
-        if (input.equals("y") || input.equals("n")) {
-            return input;
-        } else {
-            System.out.println("Invalid input. Please enter y or n.");
-            Scanner sc = new Scanner(System.in);
-            input = sc.nextLine();
-            return validateInput(input);
-        }
-    }
-
-    public int validateIntInput(int input) {
-        int max = 3;
-        if (input >= 0 && input < max) {
-            return input;
-        } else {
-            System.out.println("Invalid input. Please enter a number between 0 and " + (max - 1) + ".");
-            Scanner sc = new Scanner(System.in);
-            input = sc.nextInt();
-            return validateIntInput(input);
-        }
-    }
-
-    public void run(String[] args) throws Exception {
+    public void run(String[] args) {
 
         // Make up list of candidates to pass to vote methods.
         Candidate[] listOfCandidates = new Candidate[3];
@@ -284,98 +258,138 @@ public class Candidate_ec22801test {
         voters[1] = new Candidate_ac1235();
         voters[2] = new Candidate_ac1236();
 
+        System.out.println("List of candidates:");
+        // Ask if voters wish to display the list of candidates.
+        displayCandidates(listOfCandidates);
+
+        System.out.println("");
+        System.out.println("Voting in progress...");
         // Ask all voters to vote.
         Candidate[] ballotBox = new Candidate[voters.length];
-        // Ask if voters wish to display the list of candidates.
-        System.out.println("Do you wish to display the list of candidates? (y/n)");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        input = validateInput(input); // Remove this line if you don't want to validate input.
-        if (input.equals("y")) {
-            displayCandidates(listOfCandidates);
+        for (int i=0;i<voters.length;i++) {
+            Candidate v = voters[i].vote(listOfCandidates);
+            System.out.println(voters[i].getName() +" voted for " + v.getName() +
+                              ", whose slogan is '" + v .getSlogan() + "'.");
+            ballotBox[i] = v;
         }
 
-        String[] votersVoted = new String[voters.length];
+        System.out.println("");
+        System.out.println("Let's count the votes!");
+        // Check who received how many votes.
+        String[] candidateName = new String[listOfCandidates.length];
+        Integer[] candidateCount = new Integer[listOfCandidates.length];
+        for (int i = 0; i < listOfCandidates.length; i++) {
+            System.out.println("Processing candidate " + listOfCandidates[i].getName() + "...");
+            int count = 0;
+            for (int j = 0; j < ballotBox.length; j++) {
+                if (listOfCandidates[i].getName().equals(ballotBox[j].getName()))
+                    count++;
+            }
 
-        for (int i = 0; i < voters.length; i++) {
-            // Ask voter's name.
-            boolean nextVote = true;
-            while (nextVote) {
-                boolean isVoted = false;
-                System.out.print("What is your name? ");
-                String name = sc.nextLine();
-                System.out.println("Hello " + name + "!");
-                // Check if voter have voted before.
-                for (int j = 0; j < votersVoted.length; j++) {
-                    // Get voters in ballot box. If it's null then skip.
-                    String votersInBallot = votersVoted[j];
-                    if (votersInBallot == null) {
-                        continue;
-                    } else if (votersInBallot.equals(name)) {
-                        System.out.println("You have voted before!");
-                        isVoted = true;
-                        break;
+            // Add count and candidate name to each list, make sure the count is in order descending, handling null
+            // values.
+            for (int z = 0; z < candidateCount.length; z++) {
+                if (candidateCount[z] == null) {
+                    candidateCount[z] = count;
+                    candidateName[z] = listOfCandidates[i].getName();
+                    break;
+                } else if (count > candidateCount[z]) {
+                    for (int y = candidateCount.length - 1; y > z; y--) {
+                        candidateCount[y] = candidateCount[y - 1];
+                        candidateName[y] = candidateName[y - 1];
                     }
-                }
-                if (isVoted) {
-                    nextVote = true;
-                } else {
-                    // Check if voter's name is in the list of candidates and return position.
-                    int position = -1;
-                    for (int j = 0; j < listOfCandidates.length; j++) {
-                        if (listOfCandidates[j].getName().equals(name)) {
-                            position = j;
-                            break;
-                        }
-                    }
-                    // If voter's name is not in the list of candidates, ask for another name.
-                    if (position == -1) {
-                        System.out.println("Your name is not in the list of voters!");
-                        continue;
-                    } else {
-                        Candidate v = voters[i].vote(listOfCandidates);
-                        System.out.println(voters[position].getName() + " voted for " + v.getName() +
-                                ", whose slogan is '" + v.getSlogan() + "'.");
-                        ballotBox[i] = v;
-                        // Add voter's name to the list of voters voted.
-                        votersVoted[i] = name;
-                        nextVote = false;
-                        break;
-                    }
+                    candidateCount[z] = count;
+                    candidateName[z] = listOfCandidates[i].getName();
+                    break;
                 }
             }
         }
 
+        System.out.println("");
+        // Display the results.
+        for (int i = 0; i < candidateName.length; i++) {
+            System.out.println(candidateName[i] + " received " + candidateCount[i] + " votes.");
+        }
+
+        System.out.println("");
+        System.out.println("Who will win the election?");
         // Ask voters whose select winners they want to see
-        System.out.println("Do you want to see the winner selected by each voter? (y/n)");
-        input = sc.nextLine();
-        input = validateInput(input); // Remove this line if you don't want to validate input.
-        if (input.equals("y")) {
-            for (int i = 0; i < voters.length; i++) {
+        for (int i = 0; i < voters.length; i++) {
                 Person winner = voters[i].selectWinner(ballotBox);
                 System.out.println(voters[i].getName() + " says " + winner.getName() +
                         " wins the election.");
-            }
-        } else {
-            System.out.println("Whose opinion do you want to see?");
-            displayVoters(voters);
-            System.out.print("Enter the number of the voter: ");
-            int voterNumber = sc.nextInt();
-            voterNumber = validateIntInput(voterNumber); // Remove this line if you don't want to validate input.
-            Person winner = voters[voterNumber].selectWinner(ballotBox);
-            System.out.println(voters[voterNumber].getName() + " says " + winner.getName() +
-                    " wins the election.");
         }
-        
-        // Ask all voters to count the votes.
-        /*for (int i = 0; i < voters.length; i++) {
-            Person winner = voters[i].selectWinner(ballotBox);
-            System.out.println(voters[i].getName() + " says " + winner.getName() +
-                    " wins the election.");
-        }*/
 
-        sc.close();
+        // Check whether there is a tie.
+        int numberOfWinners = 0;
+        for (int i = 0; i < candidateCount.length; i++) {
+            if (candidateCount[i] == candidateCount[0])
+                numberOfWinners++;
+        }
 
+        System.out.println("");
+        System.out.println("Let's see who tells the truth!");
+        // Check whether voters are telling the truth. If there is a tie, check whether the voter is telling the truth.
+        for (int i = 0; i < voters.length; i++) {
+            if (numberOfWinners > 1) {
+                for (int j = 0; j < numberOfWinners; j++) {
+                    if (voters[i].selectWinner(ballotBox).getName().equals(candidateName[j])) {
+                        System.out.println(voters[i].getName() + " is telling the truth.");
+                        break;
+                    } else if (j == numberOfWinners - 1) {
+                        System.out.println(voters[i].getName() + " is lying.");
+                    }
+                }
+            } else if (voters[i].selectWinner(ballotBox).getName().equals(candidateName[0])) {
+                System.out.println(voters[i].getName() + " is telling the truth.");
+            } else {
+                System.out.println(voters[i].getName() + " is lying.");
+            }
+        }
+
+        // Display which slogan was the most popular.
+        System.out.println("");
+        System.out.println("Which slogan was the most popular?");
+        String[] sloganName = new String[listOfCandidates.length];
+        Integer[] sloganCount = new Integer[listOfCandidates.length];
+        for (int i = 0; i < listOfCandidates.length; i++) {
+            System.out.println("Processing slogan " + listOfCandidates[i].getSlogan() + "...");
+            int count = 0;
+            for (int j = 0; j < ballotBox.length; j++) {
+                if (listOfCandidates[i].getSlogan().equals(ballotBox[j].getSlogan()))
+                    count++;
+            }
+
+            // Add count and slogan name to each list, make sure the count is in order descending, handling null
+            // values.
+            for (int z = 0; z < sloganCount.length; z++) {
+                if (sloganCount[z] == null) {
+                    sloganCount[z] = count;
+                    sloganName[z] = listOfCandidates[i].getSlogan();
+                    break;
+                } else if (count > sloganCount[z]) {
+                    for (int y = sloganCount.length - 1; y > z; y--) {
+                        sloganCount[y] = sloganCount[y - 1];
+                        sloganName[y] = sloganName[y - 1];
+                    }
+                    sloganCount[z] = count;
+                    sloganName[z] = listOfCandidates[i].getSlogan();
+                    break;
+                }
+            }
+        }
+
+        System.out.println("");
+        String mostPopularSlogan = "";
+        if (numberOfWinners == 1) {
+            mostPopularSlogan = "The slogan '" + sloganName[0] + "' was the most popular.";
+            System.out.println(mostPopularSlogan);
+        } else {
+            for (int i = 0; i < numberOfWinners; i++) {
+                mostPopularSlogan = mostPopularSlogan + "'" + sloganName[i] + "' ";
+            }
+            System.out.println("Slogans " + mostPopularSlogan + "were the most popular.");
+        }
     }
 
     public static void main(String[] args) throws Exception {
